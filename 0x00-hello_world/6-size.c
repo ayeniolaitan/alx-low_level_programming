6b204b12b326ded50ed047bd090c402bba043147
ayeniolaitan <ayeniolaitan19@gmail.com>
2023-06-20 14:11:53 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line that puts function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of a int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long: %lu byte(s)\n", sizeof(long));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
